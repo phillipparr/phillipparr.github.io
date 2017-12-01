cc36a686f6606837e3aa8f71dc7767c2bc0be5a1
phillipparr <phillipparamirez@gmail.com>
2017-12-01 15:28:00 +0000

    Ugh

--- home.js
+++ home.js
@@ -1,9 +1,9 @@
 var planet = planetaryjs.planet();
 // You can remove this statement if `world-110m.json`
 // is in the same path as the HTML page:
-planet.loadPlugin(planetaryjs.plugins.earth({
-  topojson: { file: 'world-110m.json' }
-}));
+// planet.loadPlugin(planetaryjs.plugins.earth({
+//   topojson: { file: 'world-110m.json' }
+// }));
 // Make the planet fit well in its canvas
 planet.projection.scale(250).translate([250, 250]);
 var canvas = document.getElementById('globe');
